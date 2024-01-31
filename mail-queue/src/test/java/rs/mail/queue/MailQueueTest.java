408e78d3c10712e62b1341c41bb565d61801278f
Ralph Schuster <ralph.schuster@dlh.de>
2024-01-31 14:10:22 +0100

    Update to JUnit 5 and RsLibs 5.0

--- mail-queue/src/test/java/rs/mail/queue/MailQueueTest.java
+++ mail-queue/src/test/java/rs/mail/queue/MailQueueTest.java
@@ -3,14 +3,14 @@
  */
 package rs.mail.queue;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.github.cowwoc.tokenbucket.Bucket;
 
--- mail-templates/src/test/java/rs/mail/templates/cache/impl/DefaultCacheTest.java
+++ mail-templates/src/test/java/rs/mail/templates/cache/impl/DefaultCacheTest.java
@@ -3,16 +3,16 @@
  */
 package rs.mail.templates.cache.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collection;
 import java.util.Set;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import rs.mail.templates.cache.Cache;
 
